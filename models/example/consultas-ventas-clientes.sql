54aee31603420260dc42285b03bb8c2245a41c7c
bnavarretej@gmail.com <bnavarretej@gmail.com>
2024-06-07 06:24:27 +0000

    commit

---
+++ models/example/consultas-ventas-clientes.sql
@@ -0,0 +1,5 @@
+SELECT c.nombre_cliente, SUM(t.monto_total) AS total
+FROM `abraham-project.ambiente_rawzone.Ventas` AS t
+RIGHT OUTER JOIN `abraham-project.ambiente_rawzone.Clientes` AS c
+ON t.cliente_id = c.cliente_id
+GROUP BY c.nombre_cliente
